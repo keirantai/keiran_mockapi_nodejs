ae79574709302191140afea4d4f2f7a2c0b9c018
Keiran Tai <tkeiran@gmail.com>
2017-11-23 17:41:13 +0800

    Changed to measure the best path.

--- worker/worker.js
+++ worker/worker.js
@@ -12,6 +12,7 @@ class Worker {
 
 	/** Worker constructor to initialize setup */
 	constructor() {
+		this.MEASURED_BY_DISTANCE = true;
 		this.init();
 	}
 
@@ -51,14 +52,33 @@ class Worker {
 			var distances = await this.findDistances(route.path.toObject());
 			if (distances.status == 'OK') {
 				var total_distance = 0, total_time = 0;
-				for (var r in distances.rows) {
-					var row = distances.rows[r];
+				var best = 0; // always inspect from the start location which is the first result
+				var inspected = [];
+				// inspect each distance unless the best path found
+				while(distances.rows.length > inspected.length + 1) {
+					// console.log('The best spot:', best);
+					var row = distances.rows[best];
+					var lowestIdx = 0, lastLowest = null;
+					// loop through all distance and find the shortest distance or time
 					for (var e in row.elements) {
 						var el = row.elements[e];
-						total_distance += el.distance.value;
-						total_time += el.duration.value;
-						console.log(el.distance.value, el.duration.value);
+						var measurement = this.MEASURED_BY_DISTANCE ? el.distance.value : el.duration.value;
+						// console.log('measurement', measurement);
+						if (lastLowest == null || lastLowest > measurement) {
+							// avoid inspecting same location more than one time
+							if (inspected.indexOf(parseInt(e)) > -1) {
+								continue;
+							}
+							lastLowest = measurement;
+							lowestIdx = parseInt(e);
+						}
 					}
+					// console.log('lowest', lowestIdx, lastLowest);
+					var lowestElement = row.elements[lowestIdx];
+					total_distance += lowestElement.distance.value;
+					total_time += lowestElement.duration.value;
+					inspected.push(lowestIdx); // save the inspected location
+					best = lowestIdx + 1; // found the next best spot and inspect in next loop
 				}
 				route.total_distance = total_distance;
 				route.total_time = total_time;
@@ -83,17 +103,19 @@ class Worker {
 	async findDistances(path) {
 		console.log('[Info] Finding distance and time from Google Map API.');
 		try {
-			var origins = path.shift().join(',');
-			var destinations = []
+			var spots = [];
+			var startLocation = path.shift().join(',');
 			while (path.length > 0) {
-				destinations.push(path.shift().join(','));
+				spots.push(path.shift().join(','));
 			}
 			var query = {
-				origins: origins,
-				destinations: destinations.join('|'),
+				origins: startLocation + '|' + spots.join('|'),
+				destinations: spots.join('|'),
 				mode: 'driving'
 			};
+			console.log('query', JSON.stringify(query));
 			var result = await googleMapClient.distanceMatrix(query).asPromise();
+			console.log('result', result.json);
 			return result.json;
 		} catch (err) {
 			console.error(err);
