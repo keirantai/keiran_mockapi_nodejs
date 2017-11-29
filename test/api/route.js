314d4a1af05d6d1f8130295222f9ab5940492808
Keiran Tai <tkeiran@gmail.com>
2017-11-29 13:25:38 +0800

    Added new test cases for the API.

---
+++ test/api/route.js
@@ -0,0 +1,37 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let endpoint = 'localhost';
+let expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('API /route Tests', () => {
+	var token;
+
+	it('Post an array of geo-locations, it should return a token.', done => {
+		var data = [["22.372081", "114.107877"],["22.284419", "114.159510"],["22.326442", "114.167811"]];
+		chai.request(endpoint)
+			.post('/route')
+			.set('content-type', 'application/json')
+			.send(data)
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				console.log(res.body);
+				expect(res.body).to.have.property('token');
+				token = res.body.token;
+				done();
+			});
+	});
+
+	it('Get result by token, it should return status and/or result of the distance.', done => {
+		chai.request(endpoint)
+			.get('/route/' + token)
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				expect(res.body).to.have.property('status');
+				done();
+			});
+	});
+});
